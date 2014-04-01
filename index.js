25cf0bb4d533e39969ec13f4ec38c7092e037f68
Jasper van Rijbroek <jasper199069@live.nl>
2014-04-01 09:39:17 +0200

    re #18 The "this" variable was pointing to the wrong object, this has now been fixed.

--- index.js
+++ index.js
@@ -7,14 +7,14 @@
  */
 Promise = require('bluebird');
 
-module.exports.Router = require('./lib/router/router');
-module.exports.Dispatcher = require('./lib/dispatcher/dispatcher');
-module.exports.Controller = require('./lib/controller/controller');
-module.exports.ViewJson = require('./lib/view/json');
-module.exports.Model = require('./lib/model/model');
-module.exports.ModelAbstract = require('./lib/model/abstract');
-module.exports.Table = require('./lib/database/table/table');
-module.exports.Row = require('./lib/database/row/row');
-module.exports.Rowset = require('./lib/database/rowset/rowset');
+module.exports.Router           = require('./lib/router/router');
+module.exports.Dispatcher       = require('./lib/dispatcher/dispatcher');
+module.exports.Controller       = require('./lib/controller/controller');
+module.exports.ViewJson         = require('./lib/view/json');
+module.exports.Model            = require('./lib/model/model');
+module.exports.ModelAbstract    = require('./lib/model/abstract');
+module.exports.Table            = require('./lib/database/table/table');
+module.exports.Row              = require('./lib/database/row/row');
+module.exports.Rowset           = require('./lib/database/rowset/rowset');
 
-module.exports.Inflector = require('./lib/inflector/inflector');
+module.exports.Inflector        = require('./lib/inflector/inflector');
