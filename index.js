d038bca1b0a62034c9594d1627420fe9a53680ab
Jasper van Rijbroek <jasper199069@live.nl>
2014-04-01 15:09:26 +0200

    Added more abstract object to losen object and data source.

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ module.exports.Router           = require('./lib/router/router');
 module.exports.Dispatcher       = require('./lib/dispatcher/dispatcher');
 module.exports.Controller       = require('./lib/controller/controller');
 module.exports.ViewJson         = require('./lib/view/json');
+module.exports.ViewAbstract     = require('./lib/view/abstract');
 module.exports.Model            = require('./lib/model/model');
 module.exports.ModelAbstract    = require('./lib/model/abstract');
 module.exports.Table            = require('./lib/database/table/table');
 module.exports.Row              = require('./lib/database/row/row');
+module.exports.RowAbstract      = require('./lib/database/row/abstract');
 module.exports.Rowset           = require('./lib/database/rowset/rowset');
+module.exports.RowsetAbstract   = require('./lib/database/rowset/abstract');
 module.exports.Permission       = require('./lib/controller/permission/permission');
 
 module.exports.Inflector        = require('./lib/inflector/inflector');
