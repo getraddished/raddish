ced68c4b189a87376ffd4182d9b29b5915743e90
Jasper van Rijbroek <jasper@jvar.nl>
2014-07-14 10:51:15 +0200

    Updated Row object tests.

--- tests/row.js
+++ tests/row.js
@@ -1,11 +1,10 @@
 require('./base');
-var Row = ObjectManager.get('com://home/menu.database.row.item');
 var id = 0;
 
 describe('Row tests', function() {
     describe('#save()', function() {
         it('It should return an object with an ID set.', function(done) {
-            Row
+            ObjectManager.get('com://home/menu.database.row.item')
                 .then(function(row) {
                     return row.setData({
                         title: 'Test_Demo',
@@ -16,7 +15,7 @@ describe('Row tests', function() {
                     return row.save()
                 })
                 .then(function(row) {
-                    return row.data.getData();
+                    return row.getData();
                 })
                 .then(function(data) {
                     data.id.should.be.an.Integer;
@@ -28,10 +27,55 @@ describe('Row tests', function() {
         });
     });
 
+    describe('#load()', function() {
+        it('Should return an empty Row object', function(done) {
+            ObjectManager.get('com://home/menu.database.row.item')
+                .then(function(row) {
+                    return row.load();
+                })
+                .then(function(row) {
+                    row.isNew().should.be.true;
+                    (row.getData().id === null).should.be.true;
+
+                    done();
+                });
+        });
+    });
+
+    describe('#update()', function() {
+        it('Should update the received row object.', function(done) {
+            ObjectManager.get('com://home/menu.database.row.item')
+                .then(function(row) {
+                    return row.setData({
+                        id: id
+                    });
+                })
+                .then(function(row) {
+                    return row.load();
+                })
+                .then(function(row) {
+                    row.isNew().should.equal(false);
+
+                    return row.setData({
+                        title: 'blaat'
+                    });
+                })
+                .then(function(row) {
+                    return row.save();
+                })
+                .then(function(row) {
+                    row.getData().title.should.equal('blaat');
+                    row.getData().id.should.equal(id);
+
+                    done();
+                });
+        });
+    });
+
     describe('#delete()', function() {
         // We will set the data manually, when the item is received by the
-        it('It should return the object of the deleted item', function(done) {
-            Row
+        it('Should return the object of the deleted item', function(done) {
+            ObjectManager.get('com://home/menu.database.row.item')
                 .then(function(row) {
                     return row.setData({
                         id: id
