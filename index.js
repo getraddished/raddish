2bd41a317f91cec1a19ef28a92cbfa00952e1bb9
Jasper van Rijbroek <jasper@jvar.nl>
2014-07-17 11:06:05 +0200

    Renamed AbstractControllerBehavior and AbstractTableBehavior.

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ Socket                                      = require('./lib/socket/socket')
 
 module.exports                              = Raddish;
 module.exports.Authenticator                = require('./lib/dispatcher/authenticator/default');
-module.exports.AbstractContollerBehavior    = require('./lib/controller/behavior/abstract');
-module.exports.AbstractTableBehavior        = require('./lib/database/table/behavior/abstract');
+module.exports.ContollerBehavior            = require('./lib/controller/behavior/abstract');
+module.exports.TableBehavior                = require('./lib/database/table/behavior/abstract');
 module.exports.Application                  = require('./lib/application/application');
 module.exports.Filter                       = require('./lib/filter/filter');
 module.exports.Router                       = require('./lib/router/router');
