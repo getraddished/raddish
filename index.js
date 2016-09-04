5f24e66973e22d5f3edbe23bc80493ac8d41c2be
Jasper van Rijbroek <jasper@jvar.nl>
2016-09-04 16:36:46 +0200

    Removed everything in favor of version 3.0.0

--- index.js
+++ index.js
@@ -6,14 +6,9 @@
  * @since 28 March 2014
  */
 Promise                                     = global.Promise || require('bluebird');
-RaddishError                                = require('./lib/error/error');
-ObjectManager                               = require('./lib/object/manager');
-ObjectLoader                                = require('./lib/object/loader/loader');
 Raddish                                     = require('./lib/raddish/raddish');
-Socket                                      = require('./lib/socket/socket')
-Log                                         = require('./lib/log/log');
 
-module.exports                              = Raddish;
+module.exports                              = require('./lib/raddish/raddish');
 module.exports.Authenticator                = require('./lib/dispatcher/authenticator/default');
 module.exports.Application                  = require('./lib/application/application');
 module.exports.Behavior                     = require('./lib/command/behavior/behavior');
@@ -21,22 +16,20 @@ module.exports.Controller                   = require('./lib/controller/default'
 module.exports.Filter                       = require('./lib/filter/filter');
 module.exports.HttpDispatcher               = require('./lib/dispatcher/http');
 module.exports.DispatcherAbstract           = require('./lib/dispatcher/abstract');
-module.exports.Inflector                    = require('./lib/inflector/inflector');
-module.exports.Log                          = Log;
+module.exports.Log                          = require('./lib/log/log');
 module.exports.Mixin                        = require('./lib/mixin/mixin');
 module.exports.Model                        = require('./lib/model/default');
 module.exports.ModelAbstract                = require('./lib/model/abstract');
-module.exports.ObjectLoader                 = ObjectLoader;
-module.exports.ObjectManager                = ObjectManager;
+module.exports.ObjectManager                = require('./lib/object/manager');
 module.exports.Permission                   = require('./lib/controller/permission/default');
 module.exports.Plugin                       = require('./lib/plugin/plugin');
-module.exports.RaddishError                 = RaddishError;
+module.exports.RaddishError                 = require('./lib/error/error');
 module.exports.Router                       = require('./lib/router/router');
 module.exports.Row                          = require('./lib/database/row/default');
 module.exports.RowAbstract                  = require('./lib/database/row/abstract');
 module.exports.Rowset                       = require('./lib/database/rowset/default');
 module.exports.RowsetAbstract               = require('./lib/database/rowset/abstract');
-module.exports.Socket                       = Socket;
+module.exports.Socket                       = require('./lib/socket/socket');
 module.exports.Table                        = require('./lib/database/table/default');
 module.exports.ViewJson                     = require('./lib/view/json');
 module.exports.ViewFile                     = require('./lib/view/file');
--- tests/apps/demo/components/content/content.js
+++
@@ -1,8 +0,0 @@
-function ComponentMenu(request, response) {
-    ObjectManager.get('com://home/content.dispatcher.http')
-        .then(function(dispatcher) {
-            dispatcher.dispatch(request, response);
-        });
-}
-
-module.exports = ComponentMenu;
