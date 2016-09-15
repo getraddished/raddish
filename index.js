a45467ee5255622757dcd3101dbd921b8c8a6e78
Jasper van Rijbroek <jasper@jvar.nl>
2016-09-15 17:12:22 +0200

    Added latest changes.

--- index.js
+++ index.js
@@ -5,9 +5,10 @@
  * @author Jasper van Rijbroek <jasper@jvar.nl>
  * @since 28 March 2014
  */
-Promise                     = global.Promise || require('bluebird');
+Promise                         = global.Promise || require('bluebird');
 
-module.exports              = require('./lib/raddish/raddish');
-module.exports.Application  = require('./lib/application/application');
-module.exports.Plugin       = require('./lib/plugin/plugin');
-module.exports.Router       = require('./lib/router/router');
+module.exports                  = require('./lib/raddish/raddish');
+module.exports.Application      = require('./lib/application/application');
+module.exports.ObjectManager    = require('./lib/object/manager');
+module.exports.Plugin           = require('./lib/plugin/plugin');
+module.exports.Router           = require('./lib/router/router');
