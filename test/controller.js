ffc92bf502c75ab2e81cc334c5cbb0d66f7f7f9d
Jasper van Rijbroek <jasper@jvar.nl>
2017-02-26 10:48:39 +0100

    #10 - Added initial controller tests.

---
+++ test/controller.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var ObjectManager = require('../index').ObjectManager,
+    controller = ObjectManager.get('com://demo/demo.controller.demo');
+
+require('should');
+
+describe('Controller tests', function() {
+    describe('Basic controller methods/ properties', function() {
+        it('Should return a valid controller object', function() {
+            return controller
+                .then(function(controller) {
+                    controller.getView.should.be.a.Function;
+                    controller.getModel.should.be.a.Function;
+                    controller.getPermissions.should.be.a.Function;
+                    controller.execute.should.be.a.Function;
+                });
+        });
+    });
+
+    describe('Basic functionality tests', function() {
+        it('Should return a correct model Object', function() {
+            return controller
+                .then(function(controller) {
+                    return controller.getModel();
+                })
+                .then(function(model) {
+                    model.getItem.should.be.a.Function;
+                    model.state.should.be.a.Function;
+                    model.getList.should.be.a.Function;
+                    model.getTable.should.be.a.Function;
+                })
+                .catch(function(err) {
+                    console.log(err);
+                });
+        });
+    });
+});
