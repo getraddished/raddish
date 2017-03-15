0bd1f6b573e4a474cc071f1faef9a13525362269
Jasper van Rijbroek <jasper@jvar.nl>
2017-03-15 08:28:23 +0100

    Updated filter test to new method.

--- test/filter.js
+++ test/filter.js
@@ -5,88 +5,88 @@ var Filter = require('../index').Filter;
 describe('Filter tests', function() {
     describe('Basic filter tests', function() {
         it('Should have a sanitize method', function() {
-            Filter.getFilter('ascii').sanitize.should.be.a.Function;
-            Filter.getFilter('boolean').sanitize.should.be.a.Function;
-            Filter.getFilter('cmd').sanitize.should.be.a.Function;
-            Filter.getFilter('date').sanitize.should.be.a.Function;
-            Filter.getFilter('email').sanitize.should.be.a.Function;
-            Filter.getFilter('int').sanitize.should.be.a.Function;
-            Filter.getFilter('json').sanitize.should.be.a.Function;
-            Filter.getFilter('md5').sanitize.should.be.a.Function;
-            Filter.getFilter('raw').sanitize.should.be.a.Function;
-            Filter.getFilter('slug').sanitize.should.be.a.Function;
-            Filter.getFilter('string').sanitize.should.be.a.Function;
+            Filter.get('ascii').sanitize.should.be.a.Function;
+            Filter.get('boolean').sanitize.should.be.a.Function;
+            Filter.get('cmd').sanitize.should.be.a.Function;
+            Filter.get('date').sanitize.should.be.a.Function;
+            Filter.get('email').sanitize.should.be.a.Function;
+            Filter.get('int').sanitize.should.be.a.Function;
+            Filter.get('json').sanitize.should.be.a.Function;
+            Filter.get('md5').sanitize.should.be.a.Function;
+            Filter.get('raw').sanitize.should.be.a.Function;
+            Filter.get('slug').sanitize.should.be.a.Function;
+            Filter.get('string').sanitize.should.be.a.Function;
         });
         
         it('Should have a validate method', function() {
-            Filter.getFilter('ascii').validate.should.be.a.Function;
-            Filter.getFilter('boolean').validate.should.be.a.Function;
-            Filter.getFilter('cmd').validate.should.be.a.Function;
-            Filter.getFilter('date').validate.should.be.a.Function;
-            Filter.getFilter('email').validate.should.be.a.Function;
-            Filter.getFilter('int').validate.should.be.a.Function;
-            Filter.getFilter('json').validate.should.be.a.Function;
-            Filter.getFilter('md5').validate.should.be.a.Function;
-            Filter.getFilter('raw').validate.should.be.a.Function;
-            Filter.getFilter('slug').validate.should.be.a.Function;
-            Filter.getFilter('string').validate.should.be.a.Function;
+            Filter.get('ascii').validate.should.be.a.Function;
+            Filter.get('boolean').validate.should.be.a.Function;
+            Filter.get('cmd').validate.should.be.a.Function;
+            Filter.get('date').validate.should.be.a.Function;
+            Filter.get('email').validate.should.be.a.Function;
+            Filter.get('int').validate.should.be.a.Function;
+            Filter.get('json').validate.should.be.a.Function;
+            Filter.get('md5').validate.should.be.a.Function;
+            Filter.get('raw').validate.should.be.a.Function;
+            Filter.get('slug').validate.should.be.a.Function;
+            Filter.get('string').validate.should.be.a.Function;
         });
     });
 
     describe('Basic functionality tests', function() {
         it('Various validate tests', function() {
             // MD5
-            Filter.getFilter('md5').validate('ABDEF19906').should.be.false();
-            Filter.getFilter('md5').validate('098f6bcd4621d373cade4e832627b4f6').should.be.true();
+            Filter.get('md5').validate('ABDEF19906').should.be.false();
+            Filter.get('md5').validate('098f6bcd4621d373cade4e832627b4f6').should.be.true();
 
             // String
-            Filter.getFilter('string').validate(123).should.be.false();
-            Filter.getFilter('string').validate('123').should.be.true();
+            Filter.get('string').validate(123).should.be.false();
+            Filter.get('string').validate('123').should.be.true();
 
             // json
-            Filter.getFilter('json').validate('json:variable').should.be.false();
-            Filter.getFilter('json').validate('{"json":"variable"}').should.be.true();
+            Filter.get('json').validate('json:variable').should.be.false();
+            Filter.get('json').validate('{"json":"variable"}').should.be.true();
 
             // int
-            Filter.getFilter('int').validate('123').should.be.false();
-            Filter.getFilter('int').validate(123).should.be.true();
+            Filter.get('int').validate('123').should.be.false();
+            Filter.get('int').validate(123).should.be.true();
 
             // email
-            Filter.getFilter('email').validate('jasper@jvar').should.be.false();
-            Filter.getFilter('email').validate('jasper@jvar.nl').should.be.true();
+            Filter.get('email').validate('jasper@jvar').should.be.false();
+            Filter.get('email').validate('jasper@jvar.nl').should.be.true();
 
             // boolean
-            Filter.getFilter('boolean').validate('true').should.be.false();
-            Filter.getFilter('boolean').validate(true).should.be.true();
-            Filter.getFilter('boolean').validate(false).should.be.true();
+            Filter.get('boolean').validate('true').should.be.false();
+            Filter.get('boolean').validate(true).should.be.true();
+            Filter.get('boolean').validate(false).should.be.true();
 
             // date
-            Filter.getFilter('date').validate('2017-01-01').should.be.false();
-            Filter.getFilter('date').validate(new Date('01-01-2017')).should.be.true();
+            Filter.get('date').validate('2017-01-01').should.be.false();
+            Filter.get('date').validate(new Date('01-01-2017')).should.be.true();
         });
 
         it('Various sanitize tests', function() {
             // MD5
-            Filter.getFilter('md5').sanitize('12345').should.equal('12345');
+            Filter.get('md5').sanitize('12345').should.equal('12345');
 
             // string
-            Filter.getFilter('string').sanitize(12345).should.equal('12345');
+            Filter.get('string').sanitize(12345).should.equal('12345');
 
             // int
-            Filter.getFilter('int').sanitize('12345').should.equal(12345);
+            Filter.get('int').sanitize('12345').should.equal(12345);
 
             // data
-            Filter.getFilter('date').sanitize('2017-01-01').should.be.a.Date;
+            Filter.get('date').sanitize('2017-01-01').should.be.a.Date;
 
             // email
-            Filter.getFilter('email').sanitize('jasper[()@jvar.nl').should.equal('jasper@jvar.nl');
+            Filter.get('email').sanitize('jasper[()@jvar.nl').should.equal('jasper@jvar.nl');
 
-            Filter.getFilter('boolean').sanitize(true).should.equal(true);
-            Filter.getFilter('boolean').sanitize(false).should.equal(false);
-            Filter.getFilter('boolean').sanitize('1').should.equal(true);
-            Filter.getFilter('boolean').sanitize(0).should.equal(false);
-            Filter.getFilter('boolean').sanitize(1).should.equal(true);
-            Filter.getFilter('boolean').sanitize(10).should.equal(true);
+            Filter.get('boolean').sanitize(true).should.equal(true);
+            Filter.get('boolean').sanitize(false).should.equal(false);
+            Filter.get('boolean').sanitize('1').should.equal(true);
+            Filter.get('boolean').sanitize(0).should.equal(false);
+            Filter.get('boolean').sanitize(1).should.equal(true);
+            Filter.get('boolean').sanitize(10).should.equal(true);
         })
     });
 });
