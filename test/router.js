4d14b09e69c439fd811d39d2b878b1aeffc8110f
Jasper van Rijbroek <jasper@jvar.nl>
2017-04-16 10:09:57 +0200

    Improved tests.

--- test/router.js
+++ test/router.js
@@ -14,6 +14,11 @@ describe('Router tests', function() {
             Router.route.should.be.a.Function
             Router.parseRoutes.should.be.a.Function
         });
+
+        it('Should set the public path', function() {
+            Router.setPublicPath('./public');
+            Router.publicPath.should.equal('./public');
+        });
     });
 
     describe('Route tests', function() {
@@ -24,6 +29,49 @@ describe('Router tests', function() {
             route.pathname.should.equal('/index.html');
             route2.query.application.should.equal('demo');
             route2.query.component.should.equal('demo');
+        });
+
+        it('Should add a custom route', function() {
+            Router.addCustomRoute('/index.html', '/test/index.html');
+            Router.routes['/index.html'].should.equal('/test/index.html');
+        });
+
+        it('Should set multiple custom routes', function() {
+            Router.addCustomRoute({
+                '/index2.html': '/test/index.html',
+                '/index3.html': '/test/index.html'
+            });
+
+            Router.routes['/index2.html'].should.equal('/test/index.html');
+            Router.routes['/index3.html'].should.equal('/test/index.html');
+        });
+
+        it('Should correctly route a request', function(done) {
+            var response = {
+                data: null,
+                end: function(data) {
+                    data.should.equal('{"data":[],"states":{"test":{"filter":"int","unique":false,"value":null}}}');
+                    done();
+                },
+                statusCode: null,
+                setHeader: function(header, value) {
+                }
+            };
+
+            Router.route({
+                method: 'get',
+                headers: {
+                    'content-length': 0
+                },
+                url: 'http://localhost:1338/demo/demo',
+                on: function(event, callback) {
+                    if(event === 'end') {
+                        callback(null, null);
+                    }
+
+                    return this;
+                }
+            }, response)
         });
     });
 });
