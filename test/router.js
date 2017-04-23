c772a93d4498041390bd4605e55b02fd54feb111
Jasper van Rijbroek <jasper@jvar.nl>
2017-04-23 16:55:56 +0200

    Fixed bugs and improved router testing.

--- test/router.js
+++ test/router.js
@@ -1,77 +1,111 @@
 'use strict';
 
 var Router = require('../index').Router,
-    url = require('url');
+    url = require('url'),
+    response = {
+        data: null,
+        end: function(data) {
+            data.should.equal('{"data":[],"states":{"test":{"filter":"int","unique":false,"value":null}}}');
+            done();
+        },
+        statusCode: null,
+        setHeader: function(header, value) {
+        }
+    };
 
 require('should');
 
 describe('Router tests', function() {
     describe('Basic Tests', function() {
         it('Should hold basic methods', function() {
-            Router.addParseRule.should.be.a.Function;
-            Router.setPublicPath.should.be.a.Function
-            Router.addCustomRoute.should.be.a.Function
+            Router.addRoute.should.be.a.Function;
             Router.route.should.be.a.Function
-            Router.parseRoutes.should.be.a.Function
+            Router.routeRequest.should.be.a.Function
+            Router.serveFile.should.be.a.Function
+            Router.setPublicPath.should.be.a.Function
         });
 
         it('Should set the public path', function() {
             Router.setPublicPath('./public');
             Router.publicPath.should.equal('./public');
         });
+
+        it('Should have 3 routes defined by default', function() {
+            Router.routes.length.should.equal(3);
+        })
     });
 
     describe('Route tests', function() {
-        it('Should return the correct route', function() {
-            var route = Router.parseRoutes(url.parse('/', true)),
-                route2 = Router.parseRoutes(url.parse('/demo/demo', true));
+        it('Should return the matched route', function() {
+            var route = Router.match('/app/component');
+            route.path.should.equal('/{application}/{component}');
 
-            route.pathname.should.equal('/index.html');
-            route2.query.application.should.equal('demo');
-            route2.query.component.should.equal('demo');
+            route = Router.match('/app/component/view');
+            route.path.should.equal('/{application}/{component}/{view}');
+
         });
 
         it('Should add a custom route', function() {
-            Router.addCustomRoute('/index.html', '/test/index.html');
-            Router.routes['/index.html'].should.equal('/test/index.html');
+            Router.addRoute('/index.html', {
+                _redirect: '/test/index.html'
+            });
+            Router.routes.length.should.equal(4);
+
+            Router.match('/index.html').options._redirect.should.equal('/test/index.html');
         });
 
-        it('Should set multiple custom routes', function() {
-            Router.addCustomRoute({
-                '/index2.html': '/test/index.html',
-                '/index3.html': '/test/index.html'
-            });
+        it('Should successfully parse a route', function() {
+            var req = {
+                    method: 'get',
+                    headers: {
+                        'content-length': 0
+                    },
+                    url: 'http://localhost:1338/app/component/view',
+                    on: function(event, callback) {
+                        if(event === 'end') {
+                            callback(null, null);
+                        }
 
-            Router.routes['/index2.html'].should.equal('/test/index.html');
-            Router.routes['/index3.html'].should.equal('/test/index.html');
+                        return this;
+                    }
+                },
+                route = Router.routes[0];
+
+            return route
+                .execute(req)
+                .then(function(request) {
+                    request.path.should.equal('/app/component/view');
+                    request.query.application.should.equal('app');
+                });
         });
 
         it('Should correctly route a request', function(done) {
             var response = {
-                data: null,
-                end: function(data) {
-                    data.should.equal('{"data":[],"states":{"test":{"filter":"int","unique":false,"value":null}}}');
-                    done();
-                },
-                statusCode: null,
-                setHeader: function(header, value) {
-                }
-            };
-
-            Router.route({
-                method: 'get',
-                headers: {
-                    'content-length': 0
-                },
-                url: 'http://localhost:1338/demo/demo',
-                on: function(event, callback) {
-                    if(event === 'end') {
-                        callback(null, null);
+                    data: null,
+                    end: function (data) {
+                        data.should.equal('{"data":[],"states":{"test":{"filter":"int","unique":false,"value":null}}}');
+                        done();
+                    },
+                    statusCode: null,
+                    setHeader: function (header, value) {
                     }
+                },
+                req = {
+                    method: 'get',
+                    headers: {
+                        'content-length': 0
+                    },
+                    url: 'http://localhost:1338/demo/demo/demos',
+                    on: function(event, callback) {
+                        if(event === 'end') {
+                            callback(null, null);
+                        }
 
-                    return this;
-                }
-            }, response)
+                        return this;
+                    }
+                };
+
+            Router.route(req, response);
         });
     });
 });
